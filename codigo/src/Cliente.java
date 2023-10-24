328ccaccc6c04d73784f2a6e7120973676adef4a
Ana Flávia <anafcs.profissional@gmail.com>
2023-10-24 11:46:35 -0300

    Update: create Cliente.java
    Criação dos atributos e esqueleto dos métodos.

--- codigo/src/Cliente.java
+++ codigo/src/Cliente.java
@@ -1,5 +1,61 @@
+import java.util.ArrayList;
+
 public class Cliente {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ArrayList<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    public void setVeiculos(ArrayList<Veiculo> veiculos) {
+        this.veiculos = veiculos;
+    }
+
+    private String nome;
+    private String id;
+    private ArrayList<Veiculo> veiculos;
+
+    public Cliente(String nome, String id){
+        this.nome = nome;
+        this.id = id; 
+    }
+
+    public void addVeiculo(){
+
+    }
+
+    public Veiculo possuiVeiculo(String placa){
+        
+    }
+
+    public int TotalDeUsos(){
+
+    }
+
+    public double arrecadadoPorVeiculo(String placa){
+
+    }
+
+    public double arrecadadoTotal(){
+
+    }
+
+    public double arrecadadoNoMes(int mes){
+
     }
 }
