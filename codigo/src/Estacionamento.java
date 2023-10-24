88b9e8aa118811a31f7c7e72e828fd24a74e1b68
gramos22 <gabrielrmzf@gmail.com>
2023-10-24 18:02:03 -0300

    Update Estacionamento

--- codigo/src/Estacionamento.java
+++ codigo/src/Estacionamento.java
@@ -1,3 +1,28 @@
 public class Estacionamento {
-    
+    private String nome;
+    private Cliente[] id;
+    private Vaga[] vagas;
+    private int quantFileiras;
+    private int vagasPorFileira;
+
+    public Estacionamento(String nome, int fileiras, int vagasPorFileira) {}
+
+    public void addVeiculo(Veiculo veiculo, String idCli) {}
+
+    public void addCliente(Cliente cliente) {}
+
+    private void gerarVagas() {}
+
+    public void estacionar(String placa) {}
+
+    public void sair(String placa) {}
+
+    public double totalArrecadado() {}
+
+    public double arrecadacaoNoMes() {}
+
+    public double valorMedioPorUso() {}
+
+    public String top5Clientes(int mes) {}
+
 }
