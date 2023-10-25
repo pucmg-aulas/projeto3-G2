d83f360c0974b2557a8125dc35b1caa30d837c76
JulyaMoreyra <130570629+JulyaMoreyra@users.noreply.github.com>
2023-10-25 15:52:51 -0300

    Classes estacionamento e cliente

--- codigo/src/Cliente.java
+++ codigo/src/Cliente.java
@@ -1,5 +1,28 @@
 public class Cliente {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    private String nome;
+    private String id;
+    private Veiculo veiculos[];
+
+    public Cliente(String nome, String id, Veiculo veiculos[]){
+        this.nome = nome;
+        this.id = id;
+        this.veiculos = veiculos;
     }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
 }
--- codigo/src/Estacionamento.java
+++ codigo/src/Estacionamento.java
@@ -1,3 +1,39 @@
 public class Estacionamento {
+    private String nome;
+    private Cliente id[];
+    private Vaga vagas[];
+    private int quantFileiras, vagasPorFileira;
+
+    public Estacionamento(String nome, Cliente id[], Vaga vagas[], int quantFileiras, int vagasPorFileira){
+        this.nome = nome;
+        this.id = id;
+        this.vagas = vagas;
+        this.quantFileiras = quantFileiras;
+        this.vagasPorFileira = vagasPorFileira;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getQuantFileiras(){
+        return this.quantFileiras;
+    }
+
+    public void setQuantFileiras(int quantFileiras){
+        this.quantFileiras = quantFileiras;
+    }
+
+    public int getVagasPorFileiras(){
+        return this.vagasPorFileira;
+    }
+
+    public void setVagasPorFileira(int vagasPorFileira){
+        this.vagasPorFileira = vagasPorFileira;
+    }
     
 }
