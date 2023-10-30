588470c3d054dcd2b206a932f6da12702407d7e0
JulyaMoreyra <130570629+JulyaMoreyra@users.noreply.github.com>
2023-10-30 13:34:05 -0300

    atributos

--- codigo/src/UsoDeVaga.java
+++ codigo/src/UsoDeVaga.java
@@ -1,3 +1,16 @@
 public class UsoDeVaga {
-    
+    static double FRACAO_USO = 0.25;
+    static double VALOR_FRACAO = 4.0;
+    static double VALOR_MAXIMO = 50.0;
+    private Vaga vaga;
+    private LocalDateTime entrada;
+    private LocalDateTime saida;
+    private double valorPago;
+
+    public UsoDeVaga(Vaga vaga, LocalDateTime entrada, LocalDateTime saida, double valorPago){
+        this.vaga = vaga;
+        this.entrada = entrada;
+        this.saida = saida;
+        this.valorPago = valorPago;
+    }
 }
