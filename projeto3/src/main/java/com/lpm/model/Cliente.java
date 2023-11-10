7b4276448495598dbbc5cd4e82935fa5748c8ac0
gramos22 <gabrielrmzf@gmail.com>
2023-11-10 13:36:51 -0300

    wip: implementing serialization

--- projeto3/src/main/java/com/lpm/model/Cliente.java
+++ projeto3/src/main/java/com/lpm/model/Cliente.java
@@ -1,19 +1,37 @@
 package com.lpm.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Cliente {
+public class Cliente implements Serializable, IEmpacotavel {
     private String nome;
     private String id;
     private ArrayList<Veiculo> veiculos;
 
-    public String getNome() {
-        return nome;
+    public void gravar() {
+        File arq = new File(".\\projeto3\\db\\");
+        try {
+            arq.delete();
+            arq.createNewFile();
+
+            ObjectOutputStream objOutput = new ObjectOutputStream(new FileOutputStream(arq));
+            objOutput.writeObject(lista);
+            objOutput.close();
+
+        } catch (IOException erro) {
+            System.out.printf("Erro: %s", erro.getMessage());
+        }
     }
 
-    public String getId() {
-        return id;
+    }
+
+    public void ler() {
+
     }
 
     public Cliente(String nome, String id) {
@@ -23,8 +41,16 @@ public class Cliente {
         veiculos = new ArrayList<Veiculo>();
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
     public void addVeiculo(Veiculo veiculo) {
-        if(!veiculos.contains(veiculo)) {
+        if (!veiculos.contains(veiculo)) {
             veiculos.add(veiculo);
         } else {
             throw new Error("Erro: veiculo ja cadastrado para esse cliente");
@@ -35,9 +61,9 @@ public class Cliente {
         Iterator<Veiculo> iterator = veiculos.iterator();
         Veiculo aux;
 
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             aux = iterator.next();
-            if(aux.getPlaca().equalsIgnoreCase(placa)) {
+            if (aux.getPlaca().equalsIgnoreCase(placa)) {
                 return aux;
             }
         }
@@ -49,10 +75,10 @@ public class Cliente {
         Iterator<Veiculo> iterator = veiculos.iterator();
         int result = 0;
 
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             result = result + iterator.next().totalDeUsos();
         }
-        
+
         return result;
     }
 
@@ -64,10 +90,10 @@ public class Cliente {
         Iterator<Veiculo> iterator = veiculos.iterator();
         double result = 0.0;
 
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             result = result + iterator.next().totalArrecadado();
         }
-        
+
         return result;
     }
 
@@ -75,10 +101,10 @@ public class Cliente {
         Iterator<Veiculo> iterator = veiculos.iterator();
         double result = 0.0;
 
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             result = result + iterator.next().arrecadadoNoMes(mes);
         }
-        
+
         return result;
     }
 }
---
+++ projeto3/src/main/java/com/lpm/model/IEmpacotavel.java
@@ -0,0 +1,8 @@
+package com.lpm.model;
+
+public interface IEmpacotavel {
+    
+    public void gravar();
+
+    public void ler();
+}
