04b764604a73c73b0f922a0b6bee1af15ed106c5
gramos22 <gabrielrmzf@gmail.com>
2023-11-23 17:38:08 -0300

    feat: implement InfoSpot

--- projeto3/src/main/java/com/lpm/view/InfoSpot.java
+++ projeto3/src/main/java/com/lpm/view/InfoSpot.java
@@ -14,28 +14,29 @@ public class InfoSpot extends JFrame{
     private JLabel labelEntrada;
     private JButton btnSair;
     private JButton btnCancelar;
+    private JPanel panel1;
     private final InfoSpotController controller;
 
     public InfoSpot(Estacionamento estacionamentoAtual, Vaga vaga) {
+        setTitle(estacionamentoAtual.getNome());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+
+        add(panel1);
+
         controller = new InfoSpotController(this, estacionamentoAtual, vaga.getId());
         labelVaga.setText("Vaga: " + vaga.getId());
         controller.buscarDadosUsoAtual();
         labelVeiculo.setText("Veículo: " + controller.getIdVeiculo());
         labelEntrada.setText("Entrada: " + controller.getEntrada());
 
-        setTitle(estacionamentoAtual.getNome());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(250, 200);
-        setLocationRelativeTo(null);
-
-
         btnSair.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                controller.registrarSaida();
+                controller.registrarSaida(); dispose(); new HomeScreen(estacionamentoAtual).setVisible(true);
             }
         });
-
 
         btnCancelar.addActionListener(new ActionListener() {
             @Override
